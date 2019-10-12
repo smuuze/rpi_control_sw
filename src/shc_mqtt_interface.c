a2dd9ea4f04e349c158cc145d6f30fd9df27c7e4
smuuze <sebastia_n@web.de>
2019-10-12 07:34:25 +0100

    Added trace-callback to mqtt-interface module

--- src/shc_mqtt_interface.c
+++ src/shc_mqtt_interface.c
@@ -22,6 +22,10 @@
 
 #define MQTT_DEBUG_MSG				DEBUG_MSG
 
+// ---- STATIC FUNCTION PROTOTYPES ---------------------------------------------
+
+void trace_Callback(enum MQTTCLIENT_TRACE_LEVELS level, char* message);
+
 // ---- STATIC DATA -------------------------------------------------------------
 
 extern MSG_QEUE myCommandQeue;
@@ -32,10 +36,17 @@ u8 mqtt_init(MQTT_INTERFACE* p_mqtt_interface) {
 
 	MQTT_DEBUG_MSG("mqtt_init() - Initialize MQTT-Client\n");
 	MQTTClient_create(&p_mqtt_interface->client, p_mqtt_interface->host_address, p_mqtt_interface->client_id, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-		
+
 	MQTT_DEBUG_MSG("mqtt_init() - Set MQTT-Callbacks\n");
-	MQTTClient_setCallbacks(p_mqtt_interface->client, (void*)p_mqtt_interface, connectionLost_Callback, messageArrived_Callback, deliveryComplete_Callback);	
-	
+	MQTTClient_setCallbacks(p_mqtt_interface->client, (void*)p_mqtt_interface, connectionLost_Callback, messageArrived_Callback, deliveryComplete_Callback);
+
+	#ifdef DEBUG_ENABLED
+	{
+		MQTTClient_setTraceCallback(trace_Callback);
+		MQTTClient_setTraceLevel(MQTTCLIENT_TRACE_ERROR);
+	}
+	#endif
+
 	return NO_ERR;
 }
 
@@ -50,13 +61,13 @@ u8 mqtt_connect(MQTT_INTERFACE* p_mqtt_interface) {
 		MQTT_DEBUG_MSG("mqtt_connect() - Connect to SmartHomeBroker has FAILED !!! (err_code = %d)\n", err_code);
 		return ERR_CONNECT_TIMEOUT;
 
-	} else {		
+	} else {
 		MQTT_DEBUG_MSG("mqtt_connect() - Connection to SmartHomeBroker established.\n");
 	}
-	
+
 	MQTTClient_subscribe(p_mqtt_interface->client, p_mqtt_interface->topic_name, p_mqtt_interface->quality_of_service);
 	p_mqtt_interface->connection_lost = 0;
-	
+
 	return NO_ERR;
 
 }
@@ -68,7 +79,7 @@ u8 mqtt_send_message(MQTT_INTERFACE* p_mqtt_interface, STRING_BUFFER* p_msg_from
 	mqtt_message.payloadlen = p_msg_from->length;
 	mqtt_message.qos = MQTT_QOS;
 	mqtt_message.retained = 0;
-	
+
 	MQTTClient_deliveryToken message_token;
 
 	MQTT_DEBUG_MSG("mqtt_send_message() - Sending Message to Broker.\n");
@@ -81,9 +92,9 @@ u8 mqtt_send_message(MQTT_INTERFACE* p_mqtt_interface, STRING_BUFFER* p_msg_from
 		MQTT_DEBUG_MSG("mqtt_send_message() - Sending Message to SmartHomeBroker has FAILED !!! (err_code = %d)\n", err_code);
 		return ERR_SEND_MSG;
 	}
-	
+
 	MQTT_DEBUG_MSG("mqtt_send_message() - Delivery of Message succeeds.\n");
-	
+
 	return NO_ERR;
 }
 
@@ -92,18 +103,25 @@ void mqtt_keep_alive(void) {
 }
 // -------- MQTT CALLBACKs --------------------------------------------------------------
 
+void trace_Callback(enum MQTTCLIENT_TRACE_LEVELS level, char* message) {
+
+	char debugMsg[256];
+	sprintf(debugMsg, "mqtt_trace_Callback() - %s\n", message);
+	MQTT_DEBUG_MSG(debugMsg);
+}
+
 void connectionLost_Callback(void *context, char* cause) {
 
 	if (context == NULL) {
 		return;		
 	}
-	
+
 	MQTT_INTERFACE* ctx = (MQTT_INTERFACE*) context;
-	
+
 	if (ctx->connection_lost != 0) {
 		return;		
 	}
-	
+
 	MQTT_DEBUG_MSG("connectionLost_Callback() - MQTT-Connection has been lost !!! ---\n");
 	
 	ctx->connection_lost = 1;
