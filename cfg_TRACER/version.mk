36893a71f9a50e99b6e6726a83820d77efcc6486
smuuze <sebastia_n@web.de>
2020-04-02 20:17:28 +0000

    „““

---
+++ cfg_TRACER/version.mk
@@ -0,0 +1,15 @@
+
+# ------------------
+
+VERSION_MAJOR = 1
+VERSION_MINOR = 10
+
+# ------------------
+#
+# Log
+#
+# 1.10
+# - CHANGE : version number is now in a seperate file
+#
+# 1.9
+# - FIX: Opening source files in trace-parser has failed after a few minutes
